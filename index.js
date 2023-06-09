6054b7bda7cddf2374de8b80d8a63d140d355ff9
haresh <hp452444@gmail.com>
2023-06-09 13:41:31 +0530

    .env file configured

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const app = express();
-const userRoute = require('./src/routes/userRoutes.js');
-const productRoute = require('./src/routes/productRoutes.js');
-const { validateUser, checkAccess } = require('./src/library/userAuthorization.js');
-
-app.use(express.json({ urlEncoded: false }));
-app.use(cors());
 
 // .env file configuration
 const result = dotenv.config();
 if (result.error) {
     throw result.error;
 }
+
+const userRoute = require('./src/routes/userRoutes.js');
+const productRoute = require('./src/routes/productRoutes.js');
+const { validateUser, checkAccess } = require('./src/library/userAuthorization.js');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+app.use(cors());
+
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
